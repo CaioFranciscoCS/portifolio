4a27bb15452c03f0561bef678ab9a5bea599f0d6
CaioFrancisco <caiofranciscocs@gmail.com>
2023-10-27 21:42:00 -0300

    atualizacao_8

---
+++ data/data.js
@@ -0,0 +1,20 @@
+const portifolio = {
+    username: "Caio Francisco",
+    role: "Estudante",
+    social: ["./assets/imagens/github.svg", "./assets/imagens/email.svg", "./assets/imagens/linkedin.svg"],
+    resume: "Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+    "Eligendi dolor, doloribus provident obcaecati voluptatem " +
+    "nesciunt ipsa et dignissimos corporis reprehenderit laboriosam " +
+    "animi quisquam. Quam temporibus dolor provident vitae, beatae magni.",
+    
+    projects: [
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+        {name: "", tools: [], url: ""},
+    ]
+
+}
+
+export {portifolio}
